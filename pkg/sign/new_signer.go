d698157dba65cf401026c59cfeafd83ca911eb22
wbrikor <wanghao9484@dingtalk.com>
2025-01-08 16:54:18 +0800

    update dependencies & add GetRotateAccessTokenFromEndpoint

--- pkg/sign/new_signer.go
+++ pkg/sign/new_signer.go
@@ -7,11 +7,15 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
+	"strings"
 )
 
 const (
 	SIGNED_ACCESS_TOKEN_HEADER_NAME = "x-amz-access-token"
 	REFRESH_GRANT_TYPE              = "refresh_token"
+	ROTATE_GRANT_TYPE               = "client_credentials"
+	ROTATE_SCOPE                    = "sellingpartnerapi::client_credential:rotation"
 )
 
 type LwaAuthCredentials struct {
@@ -28,6 +32,13 @@ type Token struct {
 	TokenType    string `json:"token_type,omitempty"`
 	RefreshToken string `json:"refresh_token,omitempty"`
 	ExpiresIn    int    `json:"expires_in"`
+}
+
+type RotationToken struct {
+	AccessToken string `json:"access_token"`
+	TokenType   string `json:"token_type"`
+	Scope       string `json:"scope"`
+	ExpiresIn   int    `json:"expires_in"`
 }
 
 type TokenError struct {
@@ -65,6 +76,42 @@ func (t *LwaAuthCredentials) GetAccessTokenFromEndpoint(ctx context.Context) (*T
 		return nil, fmt.Errorf("error: %v", tokenError)
 	}
 	var token Token
+	if err := json.Unmarshal(buf.Bytes(), &token); err != nil {
+		return nil, err
+	}
+	return &token, nil
+}
+
+func (t *LwaAuthCredentials) GetRotateAccessTokenFromEndpoint(ctx context.Context) (*RotationToken, error) {
+	data := url.Values{}
+	data.Set("grant_type", ROTATE_GRANT_TYPE)
+	data.Set("scope", ROTATE_SCOPE)
+	data.Set("client_id", t.ClientId)
+	data.Set("client_secret", t.ClientSecret)
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, t.Endpoint, io.NopCloser(strings.NewReader(data.Encode())))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "x-www-form-urlencoded;charset=UTF-8")
+	client := http.DefaultClient
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	buf := new(bytes.Buffer)
+	_, err = io.Copy(buf, resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode >= http.StatusBadRequest {
+		var tokenError TokenError
+		if err := json.Unmarshal(buf.Bytes(), &tokenError); err != nil {
+			return nil, err
+		}
+		return nil, fmt.Errorf("error: %v", tokenError)
+	}
+	var token RotationToken
 	if err := json.Unmarshal(buf.Bytes(), &token); err != nil {
 		return nil, err
 	}
